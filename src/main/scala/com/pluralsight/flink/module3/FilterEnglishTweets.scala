b360684d4c4c26135fbfd7757bd6ca7cc7487c95
Dyno Fu <dfu@salesforce.com>
2023-12-28 21:03:24 -0800

    add twitter.properties

---
+++ src/main/scala/com/pluralsight/flink/module3/FilterEnglishTweets.scala
@@ -0,0 +1,26 @@
+package com.pluralsight.flink.module3
+
+import org.apache.flink.streaming.api.scala.{createTypeInformation, StreamExecutionEnvironment}
+import org.apache.flink.streaming.connectors.twitter.TwitterSource
+
+import java.util.Properties
+
+object FilterEnglishTweets {
+
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val props = new Properties()
+    val stream = getClass.getResourceAsStream("/twitter.properties")
+    props.load(stream)
+
+    env.addSource(new TwitterSource(props)).print()
+
+    env.execute()
+  }
+}
+
+case class Tweet(lang: String, text: String) {
+  override def toString: String = s"""Tweet{text='$text', lang='$lang'}"""
+}
